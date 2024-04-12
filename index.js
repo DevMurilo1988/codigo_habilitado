0a74c32e6869df7ba9ed4bd524c6d299103fdff5
Murilo Medeiros <murilo1988.dev@gmail.com>
2024-04-11 21:23:18 -0300

    .

---
+++ index.js
@@ -0,0 +1,27 @@
+let funcionarios = [
+    {id: 1, nome: "Murilo", habilitado: true},
+    {id: 2, nome: "Jéssica", habilitado: true},
+    {id: 3, nome: "Renan", habilitado: true},
+    {id: 4, nome: "Marlon", habilitado: false},
+    {id: 5, nome: "Ana", habilitado: false}
+];
+
+let contador = 0;
+let encontrouHabilitado = false;
+
+while ( contador < funcionarios.length ) {
+
+    let funcionario = funcionarios[contador];
+
+    if( funcionario.habilitado == true ){
+        console.log("Funcionário habilitado encontrado: " + funcionario.nome);
+        encontrouHabilitado = true;
+        break;
+    }
+
+    contador++;
+}
+
+if (!encontrouHabilitado) {
+    console.log("Nenhum funcionário habilitado encontrado");
+}
